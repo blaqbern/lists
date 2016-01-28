ecd6064267b8bbdd126dbda09e74f85cdabc729f
Chris Blackburn <burnicus8@gmail.com>
2016-01-27 22:42:45 -0500

    Set viz-filter to SHOW_ALL when adding a new item

--- src/reducers/visibilityFilter.js
+++ src/reducers/visibilityFilter.js
@@ -1,7 +1,11 @@
 const visibilityFilter = (state = 'SHOW_ALL', action) => {
   switch(action.type) {
+    case 'ADD_ITEM':
+      return 'SHOW_ALL';
+
     case 'SET_VISIBILITY_FILTER':
       return action.filter;
+
     default:
       return state;
   }
