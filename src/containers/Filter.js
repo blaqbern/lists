82626315107bb88865415f958a1b693437952387
Chris Blackburn <burnicus8@gmail.com>
2016-02-27 23:32:39 -0500

    add 'active' prop to Filter component

--- src/containers/Filter.js
+++ src/containers/Filter.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { setVisibilityFilter } from '../actions';
 
-function Filter({ filter, onFilterClick }) {
+function Filter({ active, filter, onFilterClick }) {
   return (
-    <div onClick={onFilterClick}>
+    <div
+      onClick={onFilterClick}
+      style={{ color: active ? '#6aa' : 'inherit' }}
+    >
       {filter}
     </div>
   );
@@ -12,12 +15,19 @@ function Filter({ filter, onFilterClick }) {
 const { string } = React.PropTypes;
 Filter.propTypes = { filter: string };
 
+function mapStateToProps(state, ownProps) {
+  const { filter } = ownProps;
+  return {
+    active: state.visibilityFilter === filter,
+  };
+}
+
 function mapDispatchToProps(dispatch, ownProps) {
   const { filter } = ownProps;
   return { onFilterClick: () => dispatch(setVisibilityFilter(filter)) };
 }
 
 export default connect(
-  (state, ownProps) => Object.assign({}, ownProps),
+  mapStateToProps,
   mapDispatchToProps
 )(Filter);
